91c802fa164bffc962ced0b17a655fed7c69de39
gvilaro-at-522001166142 <gabriela.vilaro@tecso.coop>
2020-10-08 20:11:38 -0300

    add test valid dress

--- primer_ejercicio.py
+++ primer_ejercicio.py
@@ -17,6 +17,14 @@ class TestError(unittest.TestCase):
 
         assert self.error == 'No results were found for your search "hola"'
 
+    def test_valid_dress(self):
+        self.driver.find_element_by_id('search_query_top').send_keys('dress')
+        self.driver.find_element_by_name('submit_search').click()
+        time.sleep(4)
+        self.valid_message = self.driver.find_element_by_xpath('//*[@id="center_column"]/h1/span[1]').text
+
+        assert self.valid_message == '"DRESS"'
+
     def tearDown(self):
         self.driver.close()
         self.driver.quit()
