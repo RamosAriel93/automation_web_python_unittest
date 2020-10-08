94d6f5559767343adfef0f173597b0ba5fa21054
gvilaro-at-522001166142 <gabriela.vilaro@tecso.coop>
2020-10-07 23:16:57 -0300

    refactor test

--- primer_ejercicio.py
+++ primer_ejercicio.py
@@ -1,16 +1,26 @@
 import time
-
+import unittest
 from selenium import webdriver
 
-driver = webdriver.Chrome('/home/gabriela/PycharmProjects/automation_proyect/chromedriver')
 
-driver.get('http://automationpractice.com/index.php')
-driver.find_element_by_id('search_query_top').send_keys('hola')
-driver.find_element_by_name('submit_search').click()
-time.sleep(4)
-error = driver.find_element_by_xpath('//*[@id="center_column"]/p').text
+class TestError(unittest.TestCase):
 
-assert error == 'No results were found for your search "hola"'
+    def setUp(self):
+        self.driver = webdriver.Chrome('/home/gabriela/PycharmProjects/automation_proyect/chromedriver')
+        self.driver.get('http://automationpractice.com/index.php')
 
-driver.close()
-driver.quit()
+    def test_message_error(self):
+        self.driver.find_element_by_id('search_query_top').send_keys('hola')
+        self.driver.find_element_by_name('submit_search').click()
+        time.sleep(4)
+        self.error = self.driver.find_element_by_xpath('//*[@id="center_column"]/p').text
+
+        assert self.error == 'No results were found for your search "hola"'
+
+    def tearDown(self):
+        self.driver.close()
+        self.driver.quit()
+
+
+if __name__ == '__main__':
+    unittest.main()
